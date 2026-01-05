e12058d6f2496b1e91cbdc4cbefb5c28a67ca5f8
azizul6817 <azizulhakim68178@gmail.com>
2026-01-05 15:41:17 +0600

    add

--- index.js
+++ index.js
@@ -25,13 +25,8 @@ admin.initializeApp({
 //! middleware-------------------------------
 app.use(
   cors({
-    origin: [
-      "http://localhost:5173",
-      "https://smart-deals-client.vercel.app" // future frontend
-    ],
+    origin: ["http://localhost:5173"],
     credentials: true,
-    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
 app.use(express.json());
